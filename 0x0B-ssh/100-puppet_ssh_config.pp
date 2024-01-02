0230aa6e764cdbe58f2f9966d6265e9a3519e1de
Quddu <Quddu@github.com>
2024-01-01 20:17:56 -0800

    Done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,14 @@
-#!/usr/bin/env bash
-# Bash script that creates an RSA key pair.
-ssh-keygen -b 4096 -N betty -f holberton
+# Modifying the client config file
+include stdlib
+
+file_line {
+  'password_auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    PasswordAuthentication no'
+  ;
+  'key_location':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '    IdentityFile ~/.ssh/school'
+}
