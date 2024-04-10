2a74a6ab367929fb528a6d7008643094e9c95afe
Quddu <Quddu@github.com>
2024-04-10 04:12:29 +0000

    Done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,22 +3,23 @@
     this module contains the function top_ten
 '''
 import requests
-from sys import argv
-
+import sys
 
 def top_ten(subreddit):
     '''
         returns the top ten posts for a given subreddit
     '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    user_agent = 'Lizzie'
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    headers = {'User-Agent': user_agent}
+    response = requests.get(url, headers=headers)
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
+        data = response.json()['data']
+        for post in data['children']:
+            print(post['data']['title'])
     except Exception:
         print(None)
 
-
 if __name__ == "__main__":
-    top_ten(argv[1])
+    subreddit = sys.argv[1]
+    top_ten(subreddit)
